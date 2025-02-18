a9068988cfe9e80df82b95e8e9c59f839dc5f998
James Gamble <jlgamble@miners.utep.edu>
2025-02-17 17:18:08 -0700

    add tokenizer.c and create blank functions within it.

---
+++ src/.#tokenizer.h
@@ -0,0 +1,1 @@
+student@systems-vm.1810:1739837155
---
+++ src/tokenizer.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int space_char(char c){
+
+  return 0;
+}
+
+int non_space_char(char c){
+
+  return 0;
+}
+
+char *token_start(char *str){
+
+  return 0;
+}
+
+char *token_terminator(char *token){
+
+  return 0;
+}
+
+int count_tokens(char *str){
+
+  return 0;
+}
+
+char *copy_str(char *inStr, short len){
+
+  return 0;
+}
+
+char **tokenize(char* str){
+
+  return 0;
+}
+
+void print_tokens(char **tokens){
+
+  return 0;
+}
+
+void free_tokens(char **tokens){
+
+  return 0;
+}
